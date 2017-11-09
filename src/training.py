f77e61cedab7a42f7f7b7606b10adb2081a332d5
Adam Saladino <adam@codingsimply.com>
2017-11-09 15:57:15 -0500

    Successfully saves trained model, loads and makes predictions on new data.

--- src/training.py
+++ src/training.py
@@ -19,25 +19,25 @@ class BasicDeepNeuralNetwork:
         y_training = training_data_df[['solution']].values
 
         # Load testing data set from csv file
-        testing_data_df = pd.read_csv('../data/testing_data.csv', dtype=float, index_col='id')
+        self.testing_data_df = pd.read_csv('../data/testing_data.csv', dtype=float, index_col='id')
 
-        x_testing = testing_data_df.drop(['solution', 'operator'], axis=1).values
-        y_testing = testing_data_df[['solution']].values
+        x_testing = self.testing_data_df.drop(['solution', 'operator'], axis=1).values
+        y_testing = self.testing_data_df[['solution']].values
 
-        x_scaler = MinMaxScaler(feature_range=(0, 1))
-        y_scaler = MinMaxScaler(feature_range=(0, 1))
+        self.x_scaler = MinMaxScaler(feature_range=(0, 1))
+        self.y_scaler = MinMaxScaler(feature_range=(0, 1))
 
-        self.x_scaled_training = x_scaler.fit_transform(x_training)
-        self.y_scaled_training = y_scaler.fit_transform(y_training)
+        self.x_scaled_training = self.x_scaler.fit_transform(x_training)
+        self.y_scaled_training = self.y_scaler.fit_transform(y_training)
 
-        self.x_scaled_testing = x_scaler.transform(x_testing)
-        self.y_scaled_testing = y_scaler.transform(y_testing)
+        self.x_scaled_testing = self.x_scaler.transform(x_testing)
+        self.y_scaled_testing = self.y_scaler.transform(y_testing)
 
         print(self.x_scaled_testing.shape)
         print(self.y_scaled_testing.shape)
 
-        print('Y values were scaled by multiplying by {:.10f} and adding {:.4f}'.format(y_scaler.scale_[0],
-                                                                                        y_scaler.min_[0]))
+        print('Y values were scaled by multiplying by {:.10f} and adding {:.4f}'.format(self.y_scaler.scale_[0],
+                                                                                        self.y_scaler.min_[0]))
 
     def load_params(self):
         # Define model parameters
@@ -83,12 +83,12 @@ class BasicDeepNeuralNetwork:
             weights = tf.get_variable(name='weights4', shape=[self.layer_3_nodes, self.number_of_outputs],
                                       initializer=tf.contrib.layers.xavier_initializer())
             biases = tf.get_variable(name='biases4', shape=[self.number_of_outputs], initializer=tf.zeros_initializer())
-            prediction = tf.nn.relu(tf.matmul(layer_3_output, weights) + biases)
+            self.prediction = tf.nn.relu(tf.matmul(layer_3_output, weights) + biases)
 
         # Training. A cost function is need to train a network
         with tf.variable_scope('cost'):
             self.y = tf.placeholder(tf.float32, shape=(None, 1))
-            self.cost = tf.reduce_mean(tf.squared_difference(prediction, self.y))
+            self.cost = tf.reduce_mean(tf.squared_difference(self.prediction, self.y))
 
         with tf.variable_scope('train'):
             self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
@@ -98,6 +98,8 @@ class BasicDeepNeuralNetwork:
             self.summary = tf.summary.merge_all()
 
     def train(self):
+        saver = tf.train.Saver()
+
         with tf.Session() as session:
             session.run(tf.global_variables_initializer())
 
@@ -129,9 +131,51 @@ class BasicDeepNeuralNetwork:
             print('Final Training Cost: {}'.format(final_training_cost))
             print('Final Testing Cost: {}'.format(final_testing_cost))
 
+            # Now that the neural network is trained, let's use it to make predictions for our test data.
+            # Pass in the X testing data and run the "prediction" operation
+            y_predicted_scaled = session.run(self.prediction, feed_dict={self.x: self.x_scaled_testing})
+
+            # Un-scale the data back to it's original units
+            y_predicted = self.y_scaler.inverse_transform(y_predicted_scaled)
+
+            real_solution = self.testing_data_df['solution'].values[0]
+            predicted_solution = y_predicted[0][0]
+
+            print("The actual solution of the first problem {}".format(real_solution))
+            print("Our neural network predicted solution of {}".format(predicted_solution))
+
+            save_path = saver.save(session, '../trained_models/trained.ckpt')
+            print("Model saved: {}".format(save_path))
+
+    def predict_solution(self, problem):
+        problem_scaled = self.x_scaler.transform(problem)
+
+        saver = tf.train.Saver()
+        # Initialize a session so that we can run TensorFlow operations
+        with tf.Session() as session:
+            # When loading from a checkpoint, don't initialize the variables!
+            # Instead, load them from disk:
+            saver.restore(session, '../trained_models/trained.ckpt')
+
+            print("Trained model loaded from disk.")
+
+            # Now that the neural network is trained, let's use it to make predictions for our test data.
+            # Pass in the X testing data and run the "prediction" operation
+            y_predicted_scaled = session.run(self.prediction, feed_dict={self.x: problem_scaled})
+
+            # Un-scale the data back to it's original units
+            y_predicted = self.y_scaler.inverse_transform(y_predicted_scaled)
+
+            real_solution = problem[0][0] + problem[0][1]
+            predicted_solution = y_predicted[0][0]
+
+            print("The actual solution of the first problem {}".format(real_solution))
+            print("Our neural network predicted solution of {}".format(predicted_solution))
+
 
 basicNeuralNetwork = BasicDeepNeuralNetwork()
 basicNeuralNetwork.load_and_scale_data()
 basicNeuralNetwork.load_params()
 basicNeuralNetwork.build_layers()
-basicNeuralNetwork.train()
+# basicNeuralNetwork.train()
+basicNeuralNetwork.predict_solution([[1000, 1000]])
